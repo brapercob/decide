bfeb215e0b142ed6a7d8998347f8982828bfabd2
brapercob <brayanpercob@gmail.com>
2022-08-18 23:01:04 +0300

    Añadidos tests de selenium en authentication test

--- decide/authentication/tests.py
+++ decide/authentication/tests.py
@@ -1,9 +1,18 @@
 from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from rest_framework.test import APIClient
 from rest_framework.test import APITestCase
 
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+from base.tests import BaseTestCase
 
 from base import mods
 
@@ -128,3 +137,31 @@ class AuthTestCase(APITestCase):
             sorted(list(response.json().keys())),
             ['token', 'user_pk']
         )
+
+class AdminTestCase(StaticLiveServerTestCase):
+
+    	def setUp(self):
+      	#Load base test functionality for decide
+      	self.base = BaseTestCase()
+      	self.base.setUp()
+
+      	options = webdriver.ChromeOptions()
+      	options.headless = True
+      	self.driver = webdriver.Chrome(options=options)
+
+      	super().setUp()            
+            
+   	def tearDown(self):           
+      	super().tearDown()
+        	self.driver.quit()
+
+        	self.base.tearDown()
+
+	def test_simpleCorrectLogin(self):                    
+        	self.driver.get(f'{self.live_server_url}/admin/')
+        	self.driver.find_element_by_id('id_username').send_keys("admin")
+        	self.driver.find_element_by_id('id_password').send_keys("qwerty",Keys.ENTER)
+        
+        	print(self.driver.current_url)
+        	#In case of a correct loging, a element with id 'user-tools' is shown in the upper right part
+        	self.assertTrue(len(self.driver.find_elements_by_id('user-tools'))==1)
