9bc9cd7cdf5c69193c55227715a5d0a5aa4382df
brapercob <brayanpercob@gmail.com>
2022-08-23 12:58:17 +0300

    Heroku: cambios en el settings

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -70,7 +70,8 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'http://localhost:8000'
+BASEURL = 'https://egc-app-decide-brapercob.herokuapp.com'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -180,3 +181,8 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+APIS = {}
+
+import django_heroku
+django_heroku.settings(locals())
